d53e6e0b7061744ad39108bdc9b594a67ab0c5d9
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-02-06 03:55:39 +0200

    added the ability to steer the car through touching thsides of the screen

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -8,10 +8,27 @@ public class Car : MonoBehaviour
     [SerializeField, Tooltip("speed increase per second")]
     private float _accerelationRate;
 
+    [SerializeField, Tooltip("Turn rate of the car")]
+    private float _turnRate;
+
+    private float _steerDirection;
+
 
     private void Update()
     {
         _speed += _accerelationRate * Time.deltaTime;
         transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+
+        SteerCar(_steerDirection);
+        Debug.Log(_steerDirection);
     }
+
+
+    private void SteerCar(float direction)
+    {
+        transform.Rotate(0.0f, direction * _turnRate * Time.deltaTime, 0.0f);
+    }
+
+
+    public float SteerDirection { set => _steerDirection = value; }
 }
