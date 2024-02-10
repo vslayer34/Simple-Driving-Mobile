9ab7e7793ff196e492589e17a5c819bf68ef94bc
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-02-10 16:59:53 +0200

    adding basic energy system

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -5,8 +6,28 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    // timed aspect of the energy recharge
+    [SerializeField, Tooltip("How much time it needs to refill enery bar")]
+    private int _energyRefillRate;
+
+
+    [SerializeField, Tooltip("Max number of energy")]
+    private int _maxEnergy;
+
+
+    private int _currentEnergy;
+
+
+    private const string _enregyKey = "Energy";
+    private const string _energyReadyKey = "Energy Ready";
+
+
     [SerializeField]
     private Button _playBtn;
+
+    [SerializeField]
+    private TextMeshProUGUI _playBtnTextField;
+
 
     [SerializeField, Tooltip("Score Display section")]
     private TextMeshProUGUI _highScoreTextField;
@@ -20,8 +41,34 @@ public class MainMenu : MonoBehaviour
         });
 
         DisplayHighestScore();
+        CheckEnergy();
     }
 
+
+    private void CheckEnergy()
+    {
+        _currentEnergy = PlayerPrefs.GetInt(_enregyKey, _maxEnergy);
+
+        if (_currentEnergy == 0)
+        {
+            string energyReadyString = PlayerPrefs.GetString(_energyReadyKey, String.Empty);
+
+            if (string.IsNullOrEmpty(_enregyKey))
+            {
+                return;
+            }
+
+            DateTime energyReady = DateTime.Parse(energyReadyString);
+
+            if (DateTime.Now > energyReady)
+            {
+                _currentEnergy = _maxEnergy;
+                PlayerPrefs.SetInt(_enregyKey, _currentEnergy);
+            }
+        }
+
+        _playBtnTextField.text = $"Play ({_currentEnergy})";
+    }
 
     private void DisplayHighestScore()
     {
